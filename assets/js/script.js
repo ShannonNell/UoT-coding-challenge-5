2ca2177e9e52ae4c2353f93f3535de1246835304
Shannon <shannon.nell92@gmail.com>
2021-05-18 11:54:35 -0400

    Text area clickable and focused

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,41 @@ var currentDayEl = document.querySelector("#currentDay");
 
 //find current date and display
 currentDayEl.textContent = moment().format("dddd, MMMM Do, YYYY");
+
+
+// //create a task
+// var createTask = function(taskText){
+//     //create task paragraph
+//     var taskP = $("<p>")
+//         .addClass("m-1")
+//         .text(taskText);
+    
+//     //append task p to parent div
+//     $(".task").append(taskP);
+// }
+
+// //task text was clicked
+// $(".task").on("click","p", function() {
+//     //get the current text of the p element
+//     var text = $(this)
+//         .text()
+//         .trim();
+
+//     //replace with new textarea
+//     var textInput = $("<textarea>")
+//         .addClass("form-control")
+//         .val(text);
+
+$(".task").on("click", function() {
+    //conver to textarea
+    var textInput = $("<textarea>")
+        .addClass("form-control col-10");
+
+    //turn div into textarea
+    $(this).replaceWith(textInput);
+
+    //auto focus for user
+    textInput.trigger("focus");
+
+    console.log("I have been clicked");
+})
